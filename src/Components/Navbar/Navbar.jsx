82dfaa63a02f72d5ebcef506f95076a9cb40f036
unknown <kellskelechi@gmail.com>
2024-04-02 20:59:02 +0100

    routing

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,31 +1,45 @@
-
-import "./navbar.css"
-import logo from "../Assets/site-logo.png"
+import "./navbar.css";
+import logo from "../Assets/site-logo.png";
+import { GiHamburgerMenu } from "react-icons/gi";
 import { TiShoppingCart } from "react-icons/ti";
-import { useState } from "react"
+import { IoClose } from "react-icons/io5";
+import { useState } from "react";
+
 const Navbar = () => {
-  const [menu,setMenu] = useState("shop")
+  const [menu, setMenu] = useState(false);
+  const [showSidebar, setShowSidebar] = useState(false);
+
+  const toggleSidebar = () => {
+    setShowSidebar(!showSidebar);
+  };
+  const closeSidebar =()=>{
+    setShowSidebar(false)
+  }
 
   return (
-    <div className='navbar'>
-        <div className="nav-logo">
-            <img src={logo} alt="" />
-            
-        </div>
-        <ul className='nav-menu'>
-            <li onClick={()=>{setMenu("Home")}}>Home {menu==="Home"?<hr/>:<></>}</li>
-            <li onClick={()=>{setMenu("Shop")}}>Shop {menu==="Shop"?<hr/>:<></>}</li>
-            <li onClick={()=>{setMenu("About")}}>About {menu==="About"?<hr/>:<></>}</li>
-            <li onClick={()=>{setMenu("Contact")}}>Contact Us {menu==="Contact"?<hr/>:<></>}</li>
+    <div className="navbar">
+        <div className="menu-toggle" onClick={toggleSidebar}>
+        <GiHamburgerMenu size={30} />
+      </div>
+      
+      <div className="nav-logo">
+        <img src={logo} alt="" />
+      </div>
+      
+      <ul className={`nav-menu ${showSidebar ? "show-sidebar" : "close-sidebar"}`}>
+        <div className="close-sidebar" onClick={closeSidebar}><IoClose  size={30} /></div>
+        <li onClick={() => setMenu("Home")}>Home {menu === "Home" ? <hr /> : <></>}</li>
+        <li onClick={() => setMenu("Shop")}>Shop {menu === "Shop" ? <hr /> : <></>}</li>
+        <li onClick={() => setMenu("About")}>About {menu === "About" ? <hr /> : <></>}</li>
+        <li onClick={() => setMenu("Contact")}>Contact Us {menu === "Contact" ? <hr /> : <></>}</li>
+      </ul>
+      <div className="nav-login">
+        <button>Log in</button>
+        <TiShoppingCart size={30} />
+        <div className="nav-cart-count">0</div>
+      </div>
+    </div>
+  );
+};
 
-        </ul>
-        <div className="nav-login">
-            <button>Log in</button>
-            <TiShoppingCart size={40} />
-            <div className="nav-cart-count">0</div>
-        </div>
-        </div>
-  )
-}
-
-export default Navbar
+export default Navbar;
